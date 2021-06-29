91bf182769bfeac91549f47870dfec229a6db567
RodriPlataHdz <85588663+RodriPlataHdz@users.noreply.github.com>
2021-06-29 15:05:58 -0500

    Add files via upload

---
+++ tom y jerry/proyecto 20/sketch.js
@@ -0,0 +1,43 @@
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
+function preload() {
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    tom = createSprite(870, 600);
+    tom.addAnimation("tomSleeping", tomImg1);
+    tom.scale = 0.2;
+
+    
+}
+
+function draw() {
+
+    background(bg);
+
+   
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        tom.velocityX = -5; 
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
+        
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
+    }
+}
